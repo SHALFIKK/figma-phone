aea019be93913086f76876366547fe14c4eabfa2
SHALFIKK <145769353+SHALFIKK@users.noreply.github.com>
2023-11-16 13:40:50 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+const sunLink = document.getElementById("sunLink");
+const moonLink = document.getElementById("moonLink");
+const body = document.body;
+const lightModeClass = "light-mode";
+const activeClass = 'active';
+const logoSvg = document.querySelector("#logo-svg");
+
+sunLink.addEventListener("click", () => {
+    body.classList.remove(lightModeClass);
+    moonLink.classList.remove(activeClass);
+    sunLink.classList.add(activeClass);
+    logoSvg.setAttribute("src", 'img/YUDAEV BRANDING.svg');
+
+
+});
+
+moonLink.addEventListener("click", () => {
+    body.classList.add(lightModeClass);
+    moonLink.classList.add(activeClass);
+    sunLink.classList.remove(activeClass);
+    logoSvg.setAttribute("src", 'img/udaev.svg.branding.svg');
+
+});
+
 const input = document.getElementById("phone-input");
 let lastValue = ""
 input.addEventListener("input", (e) => {
